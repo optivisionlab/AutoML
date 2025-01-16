7b5a44c8c02fd507bc4d8357b6bdee0743cee67a
nguyenkhanh0310 <mykhanh03102003@gmail.com>
2025-01-16 15:10:58 +0700

    Adding toast into Register page

--- src/frontend/src/redux/slices/registerSlice.ts
+++ src/frontend/src/redux/slices/registerSlice.ts
@@ -1,7 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-
 interface UserState {
   user: IUser | null;
   status: "idle" | "loading" | "succeeded" | "failed";
@@ -14,20 +13,41 @@ const initialState: UserState = {
   error: null,
 };
 
+// export const registerAsync = createAsyncThunk(
+//   "register",
+//   async (payload: IUser, thunkAPI) => {
+//     try {
+//       const response = await axios.post(`http://127.0.0.1:9999/signup`, payload)
+
+//       console.log(">> response register:", response);
+
+//       if(response.status < 400){
+//         return response.data;
+//       }
+
+//     } catch (error: any) {
+//       return thunkAPI.rejectWithValue(error ?? "Network error");
+//     }
+//   }
+// );
+
 export const registerAsync = createAsyncThunk(
   "register",
   async (payload: IUser, thunkAPI) => {
     try {
-      const response = await axios.post(`http://127.0.0.1:9999/signup`, payload)
-
-      console.log(">> response register:", response);
-
-      if(response.status < 400){
-        return response.data;
-      }
-      return thunkAPI.rejectWithValue([response.data]);
+      const response = await axios.post(
+        `http://127.0.0.1:9999/signup`,
+        payload
+      );
+      return response.data;
     } catch (error: any) {
-      return thunkAPI.rejectWithValue(error ?? "Network error");
+      if (axios.isAxiosError(error)) {
+        return thunkAPI.rejectWithValue(
+          error.response?.data || "An unexpected error just happened"
+        );
+      }
+      console.error("NETWORK ERROR:", error);
+      return thunkAPI.rejectWithValue("NETWORK ERROR or unexpected error");
     }
   }
 );
@@ -38,19 +58,19 @@ const registerSlice = createSlice({
   reducers: {},
   extraReducers(builder) {
     builder
-    .addCase(registerAsync.pending, (state) => {
-      state.status = "loading";
-      state.error = null;
-    })
-    .addCase(registerAsync.fulfilled, (state, action) => {
-      state.status = "succeeded";
-      state.user = action.payload;
-    })
-    .addCase(registerAsync.rejected, (state, action) => {
-      state.status = "failed";
-      state.error = action?.error?.message ?? "Failed to register";
-    })
+      .addCase(registerAsync.pending, (state) => {
+        state.status = "loading";
+        state.error = null;
+      })
+      .addCase(registerAsync.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.user = action.payload;
+      })
+      .addCase(registerAsync.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action?.error?.message ?? "Failed to register";
+      });
   },
-})
+});
 
 export default registerSlice.reducer;
