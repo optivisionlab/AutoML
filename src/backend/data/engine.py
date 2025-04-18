0c5b769b841c48f8806714761143805f3dfb943b
chuanh1214 <chuthianh2202chuthianh22@gmail.com>
2025-04-18 18:25:42 +0700

    fix api get list data

--- src/backend/data/engine.py
+++ src/backend/data/engine.py
@@ -10,7 +10,9 @@ import io
 db = get_database()
 data_collection = db["tbl_Data"]
 
+
 class Data(BaseModel):
+    id: str
     dataName: str
     dataType: str
     dataFile: str
@@ -19,6 +21,7 @@ class Data(BaseModel):
     list_feature: List[str]
     target: str
     userId: str
+
 
 def data_helper(data) -> dict:
     return {
@@ -32,29 +35,32 @@ def data_helper(data) -> dict:
         "target": str(data["target"]),
         "userId": str(data["userId"]),
     }
-    
+
+
 # Hàm lấy danh sách data
-def get_datas():
-    data = data_collection.find() 
+def get_datas(id_user):
+    data = data_collection.find({"userId": id_user})
     list_data = []
     for item in data:
         item["_id"] = str(item["_id"])
         list_data.append(item)
     return list_data
 
+
 def get_data_base64(id_data):
     try:
         object_id = ObjectId(id_data)
-        data = data_collection.find_one({'_id': object_id})
-        
-        if data and 'dataFile' in data:
-            return data['dataFile']
+        data = data_collection.find_one({"_id": object_id})
+
+        if data and "dataFile" in data:
+            return data["dataFile"]
         else:
             return None
 
     except Exception as e:
         print(f"Đã xảy ra lỗi khi truy vấn MongoDB: {e}")
         return None
+
 
 def decode_base64_to_dataframe(id_data):
     base64_string = get_data_base64(id_data=id_data)
@@ -69,8 +75,8 @@ def decode_base64_to_dataframe(id_data):
 
     return df
 
+
 def get_data_from_mongodb_by_id(id_data):
     df = decode_base64_to_dataframe(id_data=id_data)
-    class_names = df.iloc[:,-1].unique().tolist()
+    class_names = df.iloc[:, -1].unique().tolist()
     return df, class_names
-    
