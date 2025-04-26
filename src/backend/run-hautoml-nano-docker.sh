a5bae177fc79482bb19ef3f5bc1ede2b5171fe71
FIT HaUI <fit@QuangDM-UbuntuAI.localhost>
2025-04-26 15:00:17 +0700

    update docker

--- src/backend/run-hautoml-nano-docker.sh
+++ src/backend/run-hautoml-nano-docker.sh
@@ -2,6 +2,7 @@ docker build -t hautoml-nano -f hautoml.nano.dockerfile .
 
 docker run -it --rm --net=host \
 --name hautoml_nano_v1 \
+-v $(pwd):/app \
 --log-opt max-size=10m --log-opt max-file=3 \
--p 80:7860 \
+-p 7860:7860 \
 -d hautoml-nano python automl/demo_gradio.py
