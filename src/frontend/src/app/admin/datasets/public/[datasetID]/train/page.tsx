2b94f4b0ad043b92c9f24a037fd22b4b55c0a54d
nguyenkhanh0310 <mykhanh03102003@gmail.com>
2025-05-10 16:36:23 +0700

    Quan ly bo du lieu ca nhan, bo du lieu cong khai

---
+++ src/frontend/src/app/admin/datasets/public/[datasetID]/train/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+import TrainCard from "@/components/publicDatasetUser/TrainCard";
+
+export default function Page() {
+  const params = useParams();
+  const datasetID = Array.isArray(params?.datasetID)
+    ? params.datasetID[0]
+    : params?.datasetID;
+
+  const [dataName, setDataName] = useState<string>("Đang tải...");
+
+  useEffect(() => {
+    sessionStorage.clear();
+    
+    const fetchData = async () => {
+      try {
+        const res = await fetch(
+          `http://127.0.0.1:9999/get-data-info?id=${datasetID}`,
+          {
+            method: "POST",
+            headers: {
+              Accept: "application/json",
+            },
+            body: "",
+            cache: "no-store",
+          }
+        );
+
+        if (!res.ok) throw new Error("Lỗi khi gọi API");
+
+        const data = await res.json();
+        setDataName(data.dataName || "Không rõ");
+      } catch (error) {
+        console.error("Lỗi lấy dữ liệu:", error);
+        setDataName("Không thể tải tên bộ dữ liệu");
+      }
+    };
+
+    if (datasetID) {
+      fetchData();
+    }
+  }, [datasetID]);
+
+  return datasetID ? (
+    <TrainCard datasetID={datasetID} datasetName={dataName} />
+  ) : (
+    <div>Không tìm thấy ID dataset</div>
+  );
+}
