e258a1d27f82551569c05a4c52bbfbab332746b6
Vanh <vanhnekdungso74@gmail.com>
2025-10-05 16:48:00 +0700

    Update code Server action

--- src/frontend/src/app/serverActions/auth.ts
+++ src/frontend/src/app/serverActions/auth.ts
@@ -1,5 +1,7 @@
 "use server";
 
+import { error } from "console";
+
 // Quên mật khẩu -> gọi yêu cầu gửi
 export async function forgotPassword(email: string) {
   try {
@@ -14,7 +16,7 @@ export async function forgotPassword(email: string) {
     );
 
     if (!res.ok) {
-      return { ok: false, error: "Gửi yêu cầu thất bại" };
+      return { ok: false, error: "Email chưa được đăng kí hoặc không tồn tại" };
     }
 
     const data = await res.json();
@@ -22,4 +24,36 @@ export async function forgotPassword(email: string) {
   } catch (error: any) {
     return { ok: false, error: error.message || "Có lỗi xảy ra" };
   }
+}
+
+// Thay đổi mk
+export async function changePassword(
+  username: string,
+  password: string,
+  new1_password: string,
+  new2_password: string
+) {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_API}/change_password?username=${username}`,
+    {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password,
+        new1_password,
+        new2_password,
+      }),
+    }
+  );
+  const data = await res.json();
+
+  if (!res.ok) {
+    return { ok: false, error: data.detail || "Có lỗi xảy ra" };
+  }
+
+  return { ok: true, noError: data.detail };
 }
