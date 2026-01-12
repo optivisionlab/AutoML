681eea476b3fb7bf74609f9649b6a9e5b3655506
Vanh <vanhnekdungso74@gmail.com>
2026-01-12 23:00:11 +0700

    feat(auth): handle access/refresh token, add useApi hook

--- src/frontend/src/hooks/useUsers.ts
+++ src/frontend/src/hooks/useUsers.ts
@@ -1,6 +1,7 @@
 // useUsers gọi api lấy dữ liệu trả về
 
 import { useState, useEffect } from "react";
+import { useApi } from "./useApi";
 
 export type User = {
   _id: string;
@@ -15,13 +16,13 @@ export type User = {
 };
 
 export default function useUsers() {
+  const { get } = useApi();
   const [users, setUsers] = useState<User[]>([]);
 
   const fetchUsers = async () => {
     try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/users`);
-      const data = await res.json();
-      setUsers(data);
+      const data = await get(`${process.env.NEXT_PUBLIC_BASE_API}/users`);
+      const users = setUsers(data);
     } catch (error) {
       console.error("Failed to fetch users:", error);
     }
