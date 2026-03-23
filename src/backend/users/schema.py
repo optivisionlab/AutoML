10c8ca6802d9068b6b3404e7b025a35295851d9e
xuanndong <vuxuandong.004@gmail.com>
2026-03-23 21:47:17 +0700

    feat: set default max time and update Token class to return refresh token

--- src/backend/users/schema.py
+++ src/backend/users/schema.py
@@ -69,4 +69,9 @@ class UserResponse(BaseModel):
 
 class Token(BaseModel):
     access_token: str
+    refresh_token: str
     token_type: str = 'bearer'
+
+
+class RefreshRequest(BaseModel):
+    refresh_token: str
